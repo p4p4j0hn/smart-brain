ece2524e0910baf0f951c2d51edaa3fb84344e68
John Schmidt <john@schmidthaus.rocks>
2024-11-23 16:20:20 -0800

    feat: add my homebrew packages
    Signed-off-by: John Schmidt <john@schmidthaus.rocks>

---
+++ .github/.devcontainer/base/local-features/my-homebrew/install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash -i
+
+set -e
+
+. ./library_scripts.sh
+
+# nanolayer is a cli utility which keeps container layers as small as possible
+# source code: https://github.com/devcontainers-extra/nanolayer
+# `ensure_nanolayer` is a bash function that will find any existing nanolayer installations,
+# and if missing - will download a temporary copy that automatically get deleted at the end
+# of the script
+ensure_nanolayer nanolayer_location "v0.5.5"
+
+PACKAGES=${PACKAGES:-""}
+HOMEBREW_PACKAGES=("${PACKAGES//,/ }")
+
+for i in $HOMEBREW_PACKAGES; do
+  $nanolayer_location \
+    install \
+    devcontainer-feature \
+    "ghcr.io/devcontainers-extra/features/homebrew-package:1.0.7" \
+    --option package="$i" --option version="latest"
+done
+
+echo 'Done!'
