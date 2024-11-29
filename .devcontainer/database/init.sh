44e26559c16b68f932bb5c3d05456454f98a1fff
John Schmidt <john@schmidthaus.rocks>
2024-11-29 12:01:10 -0800

    fix: database init.sh to create smartbrain user
    Signed-off-by: John Schmidt <john@schmidthaus.rocks>

--- .devcontainer/database/init.sh
+++ .devcontainer/database/init.sh
@@ -3,9 +3,15 @@
 psql -v ON_ERROR_STOP --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
   CREATE USER smartbrain WITH PASSWORD 'smart-brain';
   CREATE DATABASE smartbrain;
+  GRANT ALL PRIVILEGES ON DATABASE smartbrain TO smartbrain;
+  GRANT USAGE ON SCHEMA public TO smartbrain;
+  GRANT ALL ON SCHEMA public TO smartbrain;
+  GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO smartbrain;
+  GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO smartbrain;
+  ALTER DATABASE smartbrain OWNER TO smartbrain;
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "smartbrain" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "smartbrain" --dbname "smartbrain" <<-EOSQL
   CREATE TABLE users (
     id serial PRIMARY KEY,
     name VARCHAR(100),
